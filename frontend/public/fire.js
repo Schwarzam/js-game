4b0d098039a5e1fe2a388e7fc3bd756ac69dfa76
schwarzam <gustavo.b.schwarz@gmail.com>
2021-04-13 10:42:07 -0300

    fix bullet angle

--- frontend/public/fire.js
+++ frontend/public/fire.js
@@ -9,16 +9,22 @@ function fireFunction(e) {
 
 function createBullet(e) {
 	let bullet = new PIXI.Sprite.from('imgs/progetil.png')
-	let a = Math.atan2(e.pageY - player[name].y, e.pageX - player[name].x);
+	let a = Math.atan2(e.pageY - 25 - player[name].y, e.pageX - player[name].x);
 
+	bullet.width = 12
+	bullet.height = 12
 	// var deltaX = input.mouseX - (this.localX + this.width/2);
 	// var deltaY = input.pageY - (this.localY + this.height/2);
 
 	bullet.player = name;
 
+	bullet.angle = a * 180/Math.PI 
+
 	bullet.anchor.set(0.5);
 	bullet.x = player[name].x;
 	bullet.y = player[name].y;
+
+	console.log(a * 180/Math.PI)
 
 	bullet.speedX =  Math.cos(a) * 5;
 	bullet.speedY = Math.sin(a) * 5;
