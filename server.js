2f03af14a0f988cf4657222673faa5c2639b13d2
schwarzam <gustavo.b.schwarz@gmail.com>
2021-04-17 23:58:28 -0300

    restarted

--- server.js
+++ server.js
@@ -5,16 +5,10 @@ const io = require('socket.io')(http);
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-app.use(express.static("frontend/public"));
+app.use(express.static("client/static"));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-const { makeid } = require('./server/utils')
-// const { startGameInterval } = require('./server/gameSocket')
-const { gameState, gameLoop, initGame, addPlayer } = require('./server/gameState')
-
-const { firingBullet } = require('./server/gun_system/firing')
-const { weapons } = require('./server/weapons')
 
 const db = require("./server/database");
 
@@ -44,7 +38,6 @@ io.on('connection', client => {
 		state[roomName] = initGame(client.id);
 		client.join(roomName)
 
-		chooseGun(client, 1)
 		state[roomName].gameMode = 'PVP'
 
 		io.sockets.in(roomName)
@@ -74,9 +67,6 @@ io.on('connection', client => {
 		}
 		client.join(gameCode);
 		client.number = numClients + 1;
-		state[gameCode] = addPlayer(state[gameCode], client.id)
-
-		chooseGun(client, 1)
 
 		var arr = []
 		for (let item of io.sockets.adapter.rooms.get(gameCode.trim()).values()){
@@ -92,66 +82,6 @@ io.on('connection', client => {
 	})
 
 	
-	client.on('mousePos', function(mousePos){
-		try{
-			const roomName = clientRooms[client.id];
-			state[roomName].players[client.id].mousePos = mousePos
-		}catch(err){
-
-		}
-	})
-	// client.on('geralConstant', function(geralConstant){
-	// 	const roomName = clientRooms[client.id];
-	// 	state[roomName].players[client.id].widthConst = geralConstant
-	// })
-
-	client.on('fireBullet', function(bullet){
-		firingBullet(bullet, client, state)
-	})
-
-	client.on('gameStatus', function(code){
-		state[code].backup = state[code]
-		state[code].backup = state[code]
-
-		for (i in Object.keys(state[code].players)){
-			state[code].alive[Object.keys(state[code].players)[i]] = 0
-		}
-
-		console.log(state[code].alive)
-
-		state[code].onGoing = true
-		io.sockets.in(code)
-			.emit('startGame', 'start');
-		startGameInterval(code);
-	})
-
-	//KEYS PRESSED 
-	client.on('keyDown', function(keyCode) {
-		const roomName = clientRooms[client.id];
-		const res = moveClient(keyCode);
-		console.log(res)
-		if (Number.isInteger(res)){
-			chooseGun(client, res)
-		}else if (res) {
-			try{
-				if (!state[roomName].players[client.id].dead){
-					if (state[roomName].players[client.id].vel.x === 0){
-						state[roomName].players[client.id].vel.x += res.x;
-					}
-					if (state[roomName].players[client.id].vel.y === 0){
-						state[roomName].players[client.id].vel.y += res.y;
-					}
-					if (Math.abs(state[roomName].players[client.id].vel.x) > 3 && Math.abs(state[roomName].players[client.id].vel.y) > 3){	
-						state[roomName].players[client.id].vel.x = state[roomName].players[client.id].vel.x/1.4
-						state[roomName].players[client.id].vel.y = state[roomName].players[client.id].vel.y/1.4
-					}
-				}
-			}catch(err){
-				console.error(err)
-			}
-		}
-	})
-
 	client.on('ping', function() {
 	    client.emit('pong');
 	});
@@ -168,123 +98,3 @@ app.get("/", (request, response) => {
 const listener = http.listen(3000, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
-
-function moveClient(keyCode){
-	try{
-		keyCode = parseInt(keyCode)
-	}catch(e){
-		console.error(e)
-		return;
-	}
-
-	switch (keyCode) {
-		case 87: { // W up
-			return { x: 0, y: -5 }
-		}case 65: { //A left
-			return { x: -5, y: 0 }
-		}case 68: { //D right
-			return { x: 5, y: 0 }
-		}case 83: { // S down
-			return { x: 0, y: 5 }
-		}case 49: {
-			return 1
-		}case 50: {
-			return 2
-		}case 51: {
-			return 3
-		}case 52: {
-			return 4
-		}
-	}
-}
-
-function startGameInterval(roomName) {
-	const intervalId = setInterval(() => {
-		const game = gameLoop(state[roomName]);
-		emitGameState(roomName, state[roomName]);
-		updateBullets(roomName)
-		if (!state[roomName].gameOver){
-			checkWin(roomName)
-		}
-	}, 1000 / 50)
-}
-
-function checkWin(roomName) {
-	if (Object.keys(state[roomName].alive).length === 1 && state[roomName].gameMode === 'PVP'){
-				state[roomName].gameOver = true
-				
-				console.log(Object.keys(state[roomName].alive)[0], 'winner')
-				io.sockets.in(roomName)
-					.emit('winner', Object.keys(state[roomName].alive)[0])
-	}
-}
-
-function emitGameState(roomName){
-	io.sockets.in(roomName)
-		.emit('gameState', state[roomName].players);
-
-	if (Object.keys(state[roomName].bullets.newBullets).length > 0){
-		io.sockets.in(roomName)
-			.emit('newBullets', state[roomName].bullets.newBullets)
-		state[roomName].bullets.newBullets = {}
-	}
-}
-
-function chooseGun(client, res){
-	try{
-		const roomName = clientRooms[client.id];
-
-		if (res > state[roomName].players[client.id].inventory.length){
-			res = state[roomName].players[client.id].inventory.length
-		}
-		const gun = state[roomName].players[client.id].inventory[res - 1]
-		state[roomName].players[client.id].gunState = weapons()[gun]
-
-		io.sockets.in(roomName)
-			.emit('changeGun', {id: client.id, url: state[roomName].players[client.id].gunState.url});
-	}catch(e){
-
-	}
-}
-
-
-function updateBullets(roomName){
-	const keys = Object.keys(state[roomName].bullets.bullets)
-	for (i in keys){
-
-		if(state[roomName].bullets.bullets[keys[i]].dead){
-				delete state[roomName].bullets.bullets[keys[i]]
-
-		}else if (state[roomName].bullets.bullets[keys[i]].posX > 1600 || state[roomName].bullets.bullets[keys[i]].posX < -100){
-			state[roomName].bullets.bullets[keys[i]].dead = true
-		}else if (state[roomName].bullets.bullets[keys[i]].posY > 1600 || state[roomName].bullets.bullets[keys[i]].posY < -100){
-			state[roomName].bullets.bullets[keys[i]].dead = true
-		}else{
-			state[roomName].bullets.bullets[keys[i]].posX += state[roomName].bullets.bullets[keys[i]].speedX;
-			state[roomName].bullets.bullets[keys[i]].posY += state[roomName].bullets.bullets[keys[i]].speedY;
-			
-			checkIfHit(roomName, state[roomName].bullets.bullets[keys[i]].posX, state[roomName].bullets.bullets[keys[i]].posY, state[roomName].bullets.bullets[keys[i]].damage, keys[i])
-		}
-	}
-	io.sockets.in(roomName)
-			.emit('bulletsState', state[roomName].bullets.bullets)
-}
-
-function checkIfHit(roomName ,BulletPosx, BulletPosy, damage, BulletId){
-	var numPlayers = io.sockets.adapter.rooms.get(roomName).size
-	var countDead = 0
-	for (i in Object.keys(state[roomName].players)){
-		
-		if (BulletPosx > state[roomName].players[Object.keys(state[roomName].players)[i]].pos.x - 13 && BulletPosx < state[roomName].players[Object.keys(state[roomName].players)[i]].pos.x + 13 && BulletPosy > state[roomName].players[Object.keys(state[roomName].players)[i]].pos.y -32 && BulletPosy < state[roomName].players[Object.keys(state[roomName].players)[i]].pos.y + 32){
-			state[roomName].players[Object.keys(state[roomName].players)[i]].health -= damage
-			state[roomName].bullets.bullets[BulletId].dead = true
-
-			if (state[roomName].players[Object.keys(state[roomName].players)[i]].health < 1){
-				state[roomName].players[Object.keys(state[roomName].players)[i]].dead = true
-
-				delete state[roomName].alive[Object.keys(state[roomName].players)[i]]
-			}
-
-		}
-	}
-}
